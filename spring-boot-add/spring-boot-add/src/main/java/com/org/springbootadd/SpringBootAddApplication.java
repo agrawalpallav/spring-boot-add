ab28f4407be58435950eaa000d5dab05f3123163
Pallav Agrawal <113340910+agrawalpallav@users.noreply.github.com>
2024-06-10 00:43:18 +0530

    Add files via upload

---
+++ spring-boot-add/spring-boot-add/src/main/java/com/org/springbootadd/SpringBootAddApplication.java
@@ -0,0 +1,13 @@
+package com.org.springbootadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAddApplication.class, args);
+	}
+
+}
